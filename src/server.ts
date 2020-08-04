eec3f2677ecce918396a3ecafe7be2ed470fe061
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-04 20:12:06 -0300

    feat: add express json parser middleware

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import morgan from 'morgan';
 const app = express();
 
 app.use(morgan('dev'));
+app.use(express.json());
 
 app.listen(5000, () => {
   console.log(`Server ins listening on port ${5000}`);
