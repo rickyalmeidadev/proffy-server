60722c57615a338152779642770ab42af22e322f
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-04 20:55:19 -0300

    feat: apply routes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import morgan from 'morgan';
+import routes from './routes';
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(routes);
 
 app.listen(5000, () => {
   console.log(`Server ins listening on port ${5000}`);
