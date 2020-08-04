0b780491bb96ce32b82d7d5266e655cb7a822f9a
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-04 20:03:20 -0300

    feat: add entry file initial config

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.listen(5000, () => {
+  console.log(`Server ins listening on port ${5000}`);
+});
