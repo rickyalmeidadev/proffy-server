2130c69f750d68fcc106c62c23fb3d5d2d60bc62
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-05 08:38:52 -0300

    refactor: handle basic errors

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -26,21 +26,28 @@ export default class ClassesController {
 
     const timeInMinutes = convertHoursToMinutes(time);
 
-    const classes = await db('classes')
-      .whereExists(function () {
-        this
-          .select('class_schedule.*')
-          .from('class_schedule')
-          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
-          .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
-          .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
-          .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes]);
-      })
-      .where('classes.subject', '=', subject)
-      .join('users', 'classes.user_id', '=', 'users.id')
-      .select(['classes.*', 'users.*']);
+    try {
+      const classes = await db('classes')
+        .whereExists(function () {
+          this
+            .select('class_schedule.*')
+            .from('class_schedule')
+            .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+            .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+            .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+            .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes]);
+        })
+        .where('classes.subject', '=', subject)
+        .join('users', 'classes.user_id', '=', 'users.id')
+        .select(['classes.*', 'users.*']);
 
-    return response.json(classes);
+      return response.json(classes);
+    } catch (error) {
+      return response.json({
+        message: 'Error while getting classes',
+        error,
+      });
+    }
   }
 
   async create(request: Request, response: Response): Promise<Response> {
--- src/controllers/ConnectionsController.ts
+++ src/controllers/ConnectionsController.ts
@@ -3,16 +3,30 @@ import db from '../database/connection';
 
 export default class ConnectionsController {
   async index(request: Request, response: Response): Promise<Response> {
-    const [total] = await db('connections').count('* as total');
+    try {
+      const [total] = await db('connections').count('* as total');
 
-    return response.json(total);
+      return response.json(total);
+    } catch (error) {
+      return response.json({
+        message: 'Error while getting total number of connections',
+        error,
+      });
+    }
   }
 
   async create(request: Request, response: Response): Promise<Response> {
     const { user_id } = request.body;
 
-    await db('connections').insert({ user_id });
+    try {
+      await db('connections').insert({ user_id });
 
-    return response.status(201).send();
+      return response.status(201).send();
+    } catch (error) {
+      return response.json({
+        message: 'Error while creating connections',
+        error,
+      });
+    }
   }
 }
