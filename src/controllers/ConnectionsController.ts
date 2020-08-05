9f59cea22c2320a748b0f6484d9626db92a0d844
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-05 08:29:25 -0300

    feat: create index method in ConnectionsController

--- src/controllers/ConnectionsController.ts
+++ src/controllers/ConnectionsController.ts
@@ -2,6 +2,12 @@ import { Request, Response } from 'express';
 import db from '../database/connection';
 
 export default class ConnectionsController {
+  async index(request: Request, response: Response): Promise<Response> {
+    const [total] = await db('connections').count('* as total');
+
+    return response.json({ total });
+  }
+
   async create(request: Request, response: Response): Promise<Response> {
     const { user_id } = request.body;
 
