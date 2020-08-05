b54c5df3957734eda55ad1eaf4ae0a979a055cdd
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-05 08:13:47 -0300

    feat: create route to list classes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import ClassesController from './controllers/ClassesController';
+import ConnectionController from './controllers/ConnectionsController';
 
 const routes = Router();
 
-const { create } = new ClassesController();
+const { create, index } = new ClassesController();
 
+routes.get('/classes', index);
 routes.post('/classes', create);
 
 export default routes;
